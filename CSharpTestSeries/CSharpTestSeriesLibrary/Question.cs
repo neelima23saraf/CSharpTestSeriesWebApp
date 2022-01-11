51f6fe53cb74e5bc3210f9d32cdeff77615fe75c
neelima23saraf <neelima23saraf@outlook.com>
2022-01-11 16:18:47 -0500

    1. Created class library 2. Changed structure of Questions table Updated DEsign Doc and the csv file

---
+++ CSharpTestSeries/CSharpTestSeriesLibrary/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTestSeriesLibrary
+{
+    internal class Question
+    {
+        public int QuestionID { get; set; }
+        public string DetailQuestion { get; set; }
+        public string Answer { get; set; }
+        public int TopicID { get; set; }
+        public string Explanation { get; set; }
+        public string  A{ get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+
+
+    }
+}
---
+++ CSharpTestSeries/CSharpTestSeriesLibrary/Topic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTestSeriesLibrary
+{
+    internal class Topic
+    {
+        public int TopicID { get; set; }
+        public string TopicName { get; set; }
+        public string  VideoName { get; set; }
+
+        public string  VideoURL { get; set; }
+    }
+}
