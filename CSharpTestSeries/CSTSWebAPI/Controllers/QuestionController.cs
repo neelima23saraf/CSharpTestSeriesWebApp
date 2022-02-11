839983bba821516c8476bd46b028c12ec52f78b2
neelima23saraf <neelima23saraf@outlook.com>
2022-02-11 13:52:59 -0500

    Implemented Paging.

--- CSharpTestSeries/CSTSWebAPI/Controllers/QuestionController.cs
+++ CSharpTestSeries/CSTSWebAPI/Controllers/QuestionController.cs
@@ -13,8 +13,7 @@ public class QuestionController : Controller
   }
 
   [HttpGet("/question/{id}")]
-  // GET: QuestionController/Details/5
-  public async Task<IActionResult> Index(int id)
+  public async Task<IActionResult> Index(int id, int pg=1)
   {
 
     List<Questions> questions = new List<Questions>();
@@ -24,20 +23,63 @@ public class QuestionController : Controller
     {
       foreach (var item in result)
       {
-        questions.Add(new Questions { Id = item.Id, 
-          Question = item.Question, 
-          Answer = item.Answer, 
-          Explanation= item.Explanation,
+        questions.Add(new Questions
+        {
+          Id = item.Id,
+          Question = item.Question,
+          Answer = item.Answer,
+          Explanation = item.Explanation,
           TopicID = item.TopicId,
           OptionA = item.OptionA,
           OptionB = item.OptionB,
           OptionC = item.OptionC,
           OptionD = item.OptionD
         });
-        
+
       }
     }
-    
-    return View(questions);
+
+    const int pageSize = 1;
+    if (pg < 1) pg = 1;
+    int recsCount = questions.Count();
+
+    var pager = new Pager(recsCount, pg, pageSize);
+
+    int recSkip = (pg - 1) * pageSize;
+
+    var data = questions.Skip(recSkip).Take(pager.PageSize).ToList();
+
+    this.ViewBag.Pager = pager;
+
+    return View(data);
+
+    //return View(questions);
   }
+  // GET: QuestionController/Details/5
+  //public async Task<IActionResult> Index(int id)
+  //{
+
+  //  List<Questions> questions = new List<Questions>();
+
+  //  var result = await _Data.GetQuestionsForATopic(id);
+  //  if (result != null)
+  //  {
+  //    foreach (var item in result)
+  //    {
+  //      questions.Add(new Questions { Id = item.Id, 
+  //        Question = item.Question, 
+  //        Answer = item.Answer, 
+  //        Explanation= item.Explanation,
+  //        TopicID = item.TopicId,
+  //        OptionA = item.OptionA,
+  //        OptionB = item.OptionB,
+  //        OptionC = item.OptionC,
+  //        OptionD = item.OptionD
+  //      });
+
+  //    }
+  //  }
+
+  //  return View(questions);
+  //}
 }
